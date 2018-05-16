955764099a268321e8396008e3d5242cba68f5a5
subhamproject <subhammandal01@gmail.com>
2018-05-16 15:09:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ export pwd=$(pwd)
 export PATH="$PATH:/usr/local/bin"
 #docker-compose run --rm  --entrypoint "mvn clean package" maven-app
 docker-compose -f test-bed.yml run --rm -w "$WORKSPACE" --entrypoint "mvn clean package" maven-app-build
+docker-compose build maven-app-image
